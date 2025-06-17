5e36ef4b960bc18ad221200645ecc889db51486c
Ryder <rdougl4@uic.edu>
2025-06-17 13:35:04 -0700

    Initial commit: backend API with event ingestion

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+)
+
+var db *pgx.Conn
+
+func main() {
+	// Connect to Postgres
+	var err error
+	db, err = pgx.Connect(context.Background(), "postgres://analytics:secret@localhost:5433/insight")
+
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer db.Close(context.Background())
+
+	app := fiber.New()
+	app.Post("/event", handleEventPost)
+	app.Listen(":3000")
+}
+
+type Event struct {
+	SessionID string `json:"session_id"`
+	Type      string `json:"type"`
+	Page      string `json:"page"`
+	X         int    `json:"x"`
+	Y         int    `json:"y"`
+	Timestamp string `json:"timestamp"`	// ISO string for now
+}
+
+func handleEventPost(c *fiber.Ctx) error {
+	var event Event
+	if err := c.BodyParser(&event); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid JSON")
+	}
+
+	_, err := db.Exec(context.Background(),
+		"INSERT INTO events (session_id, type, page, x, y, timestamp) VALUES ($1, $2, $3, $4, $5, $6)",
+		event.SessionID, event.Type, event.Page, event.X, event.Y, event.Timestamp,
+	)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Database error: %v", err))
+	}
+
+	return c.SendStatus(fiber.StatusCreated)
+}
